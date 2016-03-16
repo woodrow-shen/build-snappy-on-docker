fe28535e3935a14512b2ad62be4ec37e068df38e
Woodrow Shen <woodrow.shen@canonical.com>
2016-03-16 11:57:38 +0800

    Remove ssh-keygen

--- startup.sh
+++ startup.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-# SSH keys generation
-su jenkins
-ssh-keygen -t rsa -b 2048 -f /var/lib/jenkins/.ssh/id_rsa -N ""
-exit
-
 # Fix sudo: no tty present and no askpass program specified
 chmod +w /etc/sudoers
 echo '%jenkins ALL=(ALL:ALL) NOPASSWD:ALL' >> /etc/sudoers
