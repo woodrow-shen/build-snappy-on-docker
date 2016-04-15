5796662e2581a10629ba6af7073005724554c45a
Woodrow Shen <woodrow.shen@canonical.com>
2016-04-15 15:54:00 +0800

    Move davfs2 installation to startup.sh

--- startup.sh
+++ startup.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# install davfs2
+apt-get install -y davfs2
+
 # Fix sudo: no tty present and no askpass program specified
 chmod +w /etc/sudoers
 echo '%jenkins ALL=(ALL:ALL) NOPASSWD:ALL' >> /etc/sudoers
